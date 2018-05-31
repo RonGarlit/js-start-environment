19713888ecf51f7d0db52f2720880f9ddb429bf5
RonGarlit <rongarlit@live.com>
2018-05-31 15:04:13 -0400

    Project Structure Notes - Started Production Build-WebPack Stuff

--- buildScripts/distServer.js
+++ buildScripts/distServer.js
@@ -15,6 +15,16 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
+app.get('/users', function(req, res) {
+  // Hard coding for simplicity. Pretend this hits a real database
+  res.json([
+    {"id": 1,"firstName":"Ron","lastName":"Garlit","email":"rongarlit@live.com"},
+    {"id": 2,"firstName":"Bob","lastName":"Smith","email":"bob@gmail.com"},
+    {"id": 3,"firstName":"Tammy","lastName":"Norton","email":"tnorton@yahoo.com"},
+    {"id": 4,"firstName":"Tina","lastName":"Lee","email":"lee.tina@hotmail.com"}
+  ]);
+});
+
 app.listen(port, function(err) {
   if (err) {
     console.log(err);
